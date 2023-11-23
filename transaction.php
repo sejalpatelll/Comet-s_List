2bcceee3e9f8911eb47fc45cac0bb7d36b80fa84
YaraJan <102333657+YaraJan@users.noreply.github.com>
2023-11-23 13:30:01 -0600

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -10,4 +10,48 @@
     }
 
     header('Content-type: text/plain');
+
+    $tid = $_GET['transactionID'];
+    $bid = $_GET['buyerID'];
+    $sid = $_GET['sellerID'];
+    $count = $_GET['productCount'];
+    $tdate = date('Y-m-d');
+
+    printf($tdate);
+    printf("\n");
+
+    $products = array();
+    $productCounts = array();
+
+    for($i=0; $i<$count; $i++)
+    {
+        $currentProduct = $_GET[("productID" . $i)];
+        $currentProductCount = $_GET[("productCount" . $i)];
+        array_push($products, $currentProduct);
+        array_push($productCounts, $currentProductCount);
+    }
+
+    $sql = "INSERT INTO transaction (transaction_id, transaction_date, buyer_id, seller_id)
+            Values (?, ?, ?, ?)";
+    if($stmt = $conn->prepare($sql))
+    {
+      $stmt->bind_param("isii", $tid, $tdate, $bid, $sid);
+      $stmt->execute();
+    }
+    $stmt->close();
+
+    for($i=0; $i<$count; $i++)
+    {
+        
+    }
+
+    /*echo "<pre>";
+    print_r($products);
+    echo "</pre>";
+
+    echo "<pre>";
+    print_r($productCounts);
+    echo "</pre>";*/
+
+    $conn->close();
 ?>
