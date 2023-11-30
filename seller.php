e78ff6c77b2fe5e1132ec6dd9d4db7ee0105cf41
William Skaggs <wskaggs3262@gmail.com>
2023-11-30 13:29:36 -0600

    Change seller.php to use binding of params instead of direct insertion of values that is vulnerable to SQL injections

--- seller.php
+++ seller.php
@@ -19,21 +19,35 @@
     $businessName = $_GET['businessName'];
     $payableAccount = $_GET['payableAccount'];
 
-    $sqlNewSeller = "INSERT INTO Seller (sid, Business_Name) VALUES ('$sid', '$businessName')";
-    if ($conn->query($sqlNewSeller)==TRUE){
-      echo "Successfully added new seller";
+    $sqlNewSeller = "INSERT INTO Seller (sid, Business_Name)
+                     VALUES (?, ?)";
+    if($stmt = $conn->prepare($sqlNewSeller))
+    {
+      $stmt->bind_param("is", $sid, $businessName);
+      if($stmt->execute())
+      {
+        echo "Successfully added new seller";
+      }
+      else{
+        echo "Error: " . $sqlNewSeller . "<br>" . $conn->error;
+      }
     }
-    else {
-      echo "Error: " . $sqlNewSeller . "<br>" . $conn->error;
-    }
+    $stmt->close();
 
-    $sqlNewSellerAccount = "INSERT INTO SellerPayableAccount (SellerID, PayableAccount) VALUES ('$sid', '$payableAccount')";
-    if ($conn->query($sqlNewSellerAccount)==TRUE){
-      echo "Successfully added new seller payable account";
+    $sqlNewSellerAccount = "INSERT INTO SellerPayableAccount (SellerID, PayableAccount)
+                         VALUES (?, ?)";
+    if($stmt = $conn->prepare($sqlNewSellerAccount))
+    {
+      $stmt->bind_param("is", $sid, $payableAccount);
+      if($stmt->execute())
+      {
+        echo "Successfully added new seller payable account";
+      }
+      else{
+        echo "Error: " . $sqlNewSellerAccount . "<br>" . $conn->error;
+      }
     }
-    else {
-      echo "Error: " . $sqlNewSellerAccount . "<br>" . $conn->error;
-    }
+    $stmt->close();
 
     $conn->close();
 ?>
